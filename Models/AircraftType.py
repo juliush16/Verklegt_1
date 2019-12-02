e2db5c53fcc4bbacbe03efb8e60f67e865590e65
juliush16 <juliush16@ru.is>
2019-12-02 14:30:07 +0000

    aircraft get föll og init

--- Models/AircraftType.py
+++ Models/AircraftType.py
@@ -11,3 +11,39 @@ class Aircraftype:
         self.__length = length
         self.__height = height
         self.__wingspan = wingspan
+    
+    def __str__(self): #hvað viljum við birta 
+            pass
+
+    def get_planeTypeId(self):
+        return self.__planeTypeId
+    
+    def get_planeType(self):
+        return self.__planeType
+
+    def get_model(self):
+        return self.__model
+    
+    def get_capacity(self):
+        return self.__capacity
+    
+    def get_emptyWeight(self):
+        return self.__emptyWeight
+    
+    def get_maxTakeoffWeight(self):
+        return self.__maxTakeoffWeight
+    
+    def get_unitThrust(self):
+        return self.__unitThrust
+    
+    def get_serviceCeiling(self):
+        return self.__serviceCeiling
+    
+    def get_length(self):
+        return self.__length
+    
+    def get_height(self):
+        return self.__height
+
+    def get_wingspan(self):
+        return self.__wingspan
