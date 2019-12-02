71665ad2e735f4520c869febe87145b84ea8fca9
juliush16 <juliush16@ru.is>
2019-12-02 14:01:06 +0000

    aircraft

---
+++ Models/Aircraft.py
@@ -0,0 +1,6 @@
+class Aircraft:
+    def __init__(self,planeInsignia,planeTypeId):
+        self.__planeInsignia = planeInsignia
+        self.__planeTypeId = planeTypeId 
+
+    def __str__():
---
+++ Models/AircraftType.py
@@ -0,0 +1,13 @@
+class Aircraftype:
+    def __init__(self,planeTypeId,planeType,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan):
+        self.__planeTypeId = planeTypeId
+        self.__planeType = planeType
+        self.__model = model
+        self.__capacity = capacity
+        self.__emptyWeight = emptyWeight
+        self.__maxTakeoffWeight = maxTakeoffWeight
+        self.__unitThrust = unitThrust
+        self.__serviceCeiling = serviceCeiling
+        self.__length = length
+        self.__height = height
+        self.__wingspan = wingspan
