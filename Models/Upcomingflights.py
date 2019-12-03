fe127d48ffc3aaa8d0d6a613d50a96b1a5c0eb03
juliush16 <juliush16@ru.is>
2019-12-03 15:07:21 +0000

    upcoming

--- Models/Upcomingflights.py
+++ Models/Upcomingflights.py
@@ -1,3 +1,28 @@
 #flightNumber,departingFrom,arrivingAt,departure,arrival 
 class Upcomingflights:
-    pass
+    def __init__(self,flightNumber,departingFrom,arrivingAt,departure,arrival):
+        self.__flightNumber = flightNumber
+        self.__departingFrom = departingFrom
+        self.__arrivingAt = arrivingAt
+        self.__departure = departure
+        self.__arrival = arrival
+
+    def __str__(self): #hafa str fall eða returna öllum function með str
+        pass
+
+
+    def get_flightNumber(self):
+        return self.__flightNumber
+    
+    def get_departingFrom(self):
+        return self.__departingFrom
+
+    def get_arrivingAt(self):
+        return self.__arrivingAt
+
+    def get_departure(self):
+        return self.__departure
+    
+    def get_arrival(self):
+        return self.__arrival
+
