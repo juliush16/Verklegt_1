78c95a719711ac842a0b145797a158b06d323c2d
Dagurh17 <dagurh17@ru.is>
2019-12-03 11:23:05 +0000

    meira

--- ui/employee_ui.py
+++ ui/employee_ui.py
@@ -4,7 +4,8 @@ def menu():
         print('\n-----Register or list employees-----\n')
         print('Press "1" to register a new employee ')
         print('Press "2" to list all employees ')
-        print('Press "3" to ')
+        print('Press "3" to list all pilots')
+        print('Press "4" to list all flight attendants')
 
         print('Press "q" to Quit\n')
         choice_str = input('Choose an option: ')
@@ -12,7 +13,14 @@ def menu():
         if choice_str == 'q':
             return choice_str
         elif choice_str == '1':
-            create_new_voyage()
             choice_str = 'q'
             return choice_str
-
+        elif choice_str == '2':
+            choice_str = 'q'
+            return choice_str
+        elif choice_str == '3':
+            choice_str = 'q'
+            return choice_str
+        elif choice_str == '4':
+            choice_str = 'q'
+            return choice_str
