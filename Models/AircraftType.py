c699d21326967f4f45fc90195c27fe092d935d48
Brynjar Jonasson <brynjar19@ru.is>
2019-12-03 11:19:30 +0000

    breytingar

--- Models/AircraftType.py
+++ Models/AircraftType.py
@@ -1,13 +1,13 @@
 class Aircraftype:
-    def __init__(self,planeTypeId,planeType,model,capacity,emptyWeight,maxTakeoffWeight,unitThrust,serviceCeiling,length,height,wingspan):
-        self.__planeTypeId = planeTypeId
-        self.__planeType = planeType
+    def __init__(self,plane_type_id,plane_type,model,capacity,empty_weight,max_takeoff_weight,unit_thrust,service_ceiling,length,height,wingspan):
+        self.__plane_type_id = plane_type_id
+        self.__plane_type = plane_type
         self.__model = model
         self.__capacity = capacity
-        self.__emptyWeight = emptyWeight
-        self.__maxTakeoffWeight = maxTakeoffWeight
-        self.__unitThrust = unitThrust
-        self.__serviceCeiling = serviceCeiling
+        self.__empty_weight = empty_weight
+        self.__max_takeoff_weight = max_takeoff_weight
+        self.__unit_thrust = unit_thrust
+        self.__service_ceiling = service_ceiling
         self.__length = length
         self.__height = height
         self.__wingspan = wingspan
@@ -15,10 +15,10 @@ class Aircraftype:
     def __str__(self): #hvað viljum við birta 
             pass
 
-    def get_planeTypeId(self):
+    def get_plane_type_id(self):
         return self.__planeTypeId
     
-    def get_planeType(self):
+    def get_plane_type(self):
         return self.__planeType
 
     def get_model(self):
@@ -27,16 +27,16 @@ class Aircraftype:
     def get_capacity(self):
         return self.__capacity
     
-    def get_emptyWeight(self):
+    def get_empty_weight(self):
         return self.__emptyWeight
     
-    def get_maxTakeoffWeight(self):
+    def get_max_takeoff_weight(self):
         return self.__maxTakeoffWeight
     
-    def get_unitThrust(self):
+    def get_unit_thrust(self):
         return self.__unitThrust
     
-    def get_serviceCeiling(self):
+    def get_service_ceiling(self):
         return self.__serviceCeiling
     
     def get_length(self):
