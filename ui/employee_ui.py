94911611a7d81516d2fcbdbf764909c4ef9cd519
hildurgudrun <54936037+hildurgudrun@users.noreply.github.com>
2019-12-06 11:07:27 +0000

    n

--- ui/employee_ui.py
+++ ui/employee_ui.py
@@ -9,6 +9,7 @@ def menu():
         print('Press "3" to list all Pilots')
         print('Press "4" to list all Cabin Crew')
         print('Press "5" to find employee by SSN')
+        print('Press "6" to update employee info')
 
         print('Press "q" to Quit\n')
         choice_str = input('Choose an option: ')
@@ -30,6 +31,10 @@ def menu():
             employee_ssn = input('Please input employee SSN :')
             temp = Emp_Repo()
             print(temp.get_by_ssn(employee_ssn))
+        elif choice_str == '6':
+            employee_ssn = input('Please input employee SSN :')
+            temp = Emp_Repo()
+            temp.update_employee(employee_ssn)
 
 
 def reg_employee_menu():
@@ -38,9 +43,45 @@ def reg_employee_menu():
     while new_emp_SSN.isdigit() == False:
         new_emp_SSN = input('Enter employee social security number :')
     new_emp_name = input('Enter employee name :').capitalize()
-    new_emp_role = input('Enter employee role :').capitalize()
-    new_emp_rank = input('Enter employee rank :').capitalize()
-    new_emp_licence = input('Enter employee licence :')
+    new_emp_role = ""
+    new_emp_rank = "" 
+    new_emp_licence = ""
+    while new_emp_role != '1' or '2': #1 er pilot og 2 er cabin crew
+        new_emp_role = input('Choose an option for employee role\n1:Pilot\n2:Cabincrew\n')
+        if new_emp_role == '1':
+            new_emp_role = "Pilot"
+            while new_emp_role != '1' or '2': #pilot rank (captain or copilot)
+                new_emp_rank = input('Choose an option for employee rank\n1:Captain\n2:Copilot\n')
+                if new_emp_rank == '1':
+                    new_emp_rank = 'Captain'
+                elif new_emp_rank == '2':
+                    new_emp_rank = "Copilot"
+                break #fer ut ur while loopunni og i næstu
+            
+            while new_emp_licence != '1' or '2' or '3': #licence
+                new_emp_licence = input('Choose an option for employee licence\n1:NABAE146\n2:NAFokkerF28\n3:NAFokkerF100\n')
+                if new_emp_licence == '1':
+                    new_emp_licence = "NABAE146"
+                elif new_emp_licence == '2':
+                    new_emp_licence = "NAFokkerF28"
+                elif new_emp_licence == '3':
+                    new_emp_licence = "NAFokkerF100"
+                break #fer út ur while
+            break #fer ut ur if
+
+        elif new_emp_role == '2':
+            new_emp_role = "Cabincrew"
+            new_emp_licence = "N/A"
+            while new_emp_rank != '1' or '2': #cabincrew rank (flight service manager og flight attendant)
+                new_emp_rank = input('Choose an option for employee rank\n1:Flight Service Manager\n2:Flight Attendant\n')
+                if new_emp_rank == '1':
+                    new_emp_rank = "Flight Service Manager"
+                elif new_emp_rank == '2':
+                    new_emp_rank = "Flight Attendant"
+                break
+            break
+
+#    new_emp_licence = input('Enter employee licence :') #á bara við pilot, pilot velur á milli flugvélar / cabincrew skrifast N/A
     new_emp_address = input('Enter employee address :').capitalize()
     new_emp_phonenumber = input('Enter employee phone number :')
     new_emp_email = input('Enter employee email :')
