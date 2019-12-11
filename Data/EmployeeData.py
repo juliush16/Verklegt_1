5eb1254c8ccfb8a8d2af58fb1e564fad281a7133
annaher18 <54937171+annaher18@users.noreply.github.com>
2019-12-11 14:04:40 +0000

    kennarahjalp

--- Data/EmployeeData.py
+++ Data/EmployeeData.py
@@ -8,11 +8,10 @@ clear = lambda: os.system('cls')
 class EmployeeData:
 
     def __init__(self):
-        self.__employee = []
-        self.get_employee() #þessi
+        pass
 
     def add_employee(self, employee):
-        with open("./Repo/Employee.csv", "a+") as employee_file:
+        with open("./Repo/Employee.csv", "a") as employee_file:
             ssn = employee.get_ssn_str()
             name = employee.get_name_str()
             role = employee.get_role()
@@ -21,24 +20,35 @@ class EmployeeData:
             address =  employee.get_address()
             phonenumber = employee.get_phonenumber()
             email = employee.get_email()
-            employee_file.write("{},{},{},{},{},{},{},{},\n".format(ssn,name,
+            employee_file.write("{},{},{},{},{},{},{},{}\n".format(ssn,name,
             role,rank,licence,address,phonenumber,email))
-            self.__employee.append(employee) #þessi
 
     def get_employee(self):
-        if self.__employee == []:
-            with open("./Repo/Employee.csv", "r", encoding = "utf-8") as employee_file:
-                employee_reader = csv.reader(employee_file)
-                for line in employee_reader:
-                    ssn = line[0]
-                    name = line[1]
-                    role = line[2]
-                    rank = line[3]
-                    licence = line[4]
-                    address = line[5]
-                    phonenumber = line[6]
-                    email = line[7]
-                    new_employee = Employee(ssn, name, role, rank,licence,
-                    address, phonenumber, email)
-                    self.__employee.append(new_employee)
-        return self.__employee
+        emps = []
+        with open("./Repo/Employee.csv", "r", encoding = "utf-8", newline="") as employee_file:
+            employee_reader = csv.reader(employee_file)
+            for line in employee_reader:
+                ssn = line[0]
+                name = line[1]
+                role = line[2]
+                rank = line[3]
+                licence = line[4]
+                address = line[5]
+                phonenumber = line[6]
+                email = line[7].strip()
+                new_employee = Employee(ssn, name, role, rank,licence,
+                address, phonenumber, email)
+                emps.append(new_employee)
+        return emps
+    
+    def update_employee(self, updatedEmp):
+        allEmps = self.get_employee()
+        with open("./Repo/Employee.csv", "w+", newline="") as employee_file:
+            fieldnames = ["ssn","name","role","rank","licence","address","phonenumber","email"]
+            writer = csv.DictWriter(employee_file, fieldnames=fieldnames)
+            writer.writeheader()
+        for emp in allEmps:
+            if emp.ssn == updatedEmp.ssn:
+                self.add_employee(updatedEmp)
+            else:
+                self.add_employee(emp)
