777820fc2169de972aa965cd49cf0747fb2b8049
juliush16 <juliush16@ru.is>
2019-12-13 14:43:32 +0000

    stafsetninga villa

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -12,7 +12,7 @@ class Main:
         while choice_str != 'q':
             print('Welcome!\n')
             print('Press "1" for Voyage menu')
-            print('Press "2" to Assign eployees to a voyage')
+            print('Press "2" to Assign employees to a voyage')
             print('Press "3" to register,list or change employee information ')
             print('Press "4" for Destination Menu')
             #print('Press "4" to create a new destination ') # Breyta þessu í destination menu þar sem hægt er að sja og breyta 
